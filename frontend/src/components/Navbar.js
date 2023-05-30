adaca96b6f312290fffab3d1344df1c2e1eb663c
berialx <emailbereket@gmail.com>
2023-05-30 20:32:03 +0300

    Frontend Menu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
   return (
     <>
         <nav className="navbar">
@@ -22,7 +36,33 @@ function Navbar() {
                             Home    
                         </Link> 
                     </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            My Cart    
+                        </Link> 
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/Men's" className='nav-links' onClick={closeMobileMenu}>
+                            Men's    
+                        </Link> 
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/Women's" className='nav-links' onClick={closeMobileMenu}>
+                            Women's    
+                        </Link> 
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Phones & Laptops' className='nav-links' onClick={closeMobileMenu}>
+                            Phones & Laptops    
+                        </Link> 
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/Home Appliances' className='nav-links' onClick={closeMobileMenu}>
+                            Home Appliances    
+                        </Link> 
+                    </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>Return Home</Button>}
             </div>
         </nav>
     </>
